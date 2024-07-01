f934413efc6ec28c25f0f76801091ff6525ca9a1
llbasull <bashumoulik@gmail.com>
2024-07-01 15:11:00 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var arr = [10, 20, 20, 10, 10, 20, 5, 20]
+var count=0;
+var arr2 = []
+for(i=0;i<8;i++){
+    for(j=0;j<8;j++){
+        if(i==j){
+            continue;
+        }
+        if(arr[i]==arr[j]){
+            arr[count++]=
+        }
+    }
+}
+
+
+
